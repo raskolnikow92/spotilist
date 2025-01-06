249be8ae08a528e84e5a6c4550aab8d04ab789c1
Kai Schuleit <kai.schuleit@hotmail.de>
2025-01-06 14:50:33 +0100

    create repo

---
+++ spotilist/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import SearchBar from './components/SearchBar'
+function App() {
+  
+
+  return (
+    <>
+    <div className='backgroundimg'>
+      
+      <SearchBar />
+    </div>
+      
+
+    </>
+  )
+}
+
+export default App
---
+++ spotilist/src/components/SearchBar.tsx
@@ -0,0 +1,9 @@
+function Searchbar(){
+    return (
+        <div>
+            <input type="text" placeholder="Search for a song" />
+        </div>
+    )
+}
+
+export default Searchbar;
